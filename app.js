84cfad63728b0f11f7d5c51e24721985093eb1bb
Chelsea DuFour <chelseadufour@gmail.com>
2016-08-02 15:55:55 -0400

    commits

--- app.js
+++ app.js
@@ -10,19 +10,6 @@ app.use(express.static(__dirname + '/public'));
 //get the port, OR 5000
 app.set('port', (process.env.PORT || 5000));
 
-// var lyrics = ['song lyrics', 'singing song', 'this is a song'];
-
-
-//server only responds to requests - when we receive a request to the end point '/' send back a selection of lyrics
-// app.get('/', function(request, response){
-//   res.send(lyrics[selection]);
-// })
-
-// app.get('/api/lyrics', function (req, res) {
-//   var selection = Math.floor(Math.random()*lyrics.length);
-//   res.send(ex[selection]); /* string from array is going to be the response */
-// });
-
 //respond to a request at this end point (api/lyrics) when a request is made to api/lyrics we're going to respond by running this callback function
 //we have control over the response here, NOT the request
 app.get('/api/lyrics', function (request, response){
