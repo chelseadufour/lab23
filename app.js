8e6947c1088eb2fc61d774f74d43035902fbcedf
Chelsea DuFour <chelseadufour@gmail.com>
2016-08-02 15:53:17 -0400

    changes

--- app.js
+++ app.js
@@ -23,13 +23,17 @@ app.set('port', (process.env.PORT || 5000));
 //   res.send(ex[selection]); /* string from array is going to be the response */
 // });
 
-app.get('/api/lyrics', function (req, res){
+//respond to a request at this end point (api/lyrics) when a request is made to api/lyrics we're going to respond by running this callback function
+//we have control over the response here, NOT the request
+app.get('/api/lyrics', function (request, response){
   var selection = Math.floor(Math.random()*ex.length);
   res.send(ex[selection]);
 });
 
+//app.listen method takes the port we want to listen on and a function to run once you start the server
+//set to server variable to use
 
-var server = app.listen(app.get('port'), function () {
-  var port = server.address().port;
+app.listen(app.get('port'), function () {
+  // var port = server.address().port;
   console.log('Example app listening on port 3000!', app.get('port'));
 });
